0167c5b0af8901b2906542f306b0aabbff73783d
arisdwii <ardwisaptra@gmail.com>
2025-05-08 12:54:32 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Frontend Mentor - Intro section with dropdown navigation
+
+![Design preview for the Intro section with dropdown navigation coding challenge](./design/desktop-preview.jpg)
+
+This project is the result of a challenge from the website Frontend Mentor (frontendmentor.io).
+
+> https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5
---
+++ script.js
@@ -0,0 +1,40 @@
+const hamburger = document.querySelector(".hamburger");
+const navGroups = document.querySelectorAll(".nav-group");
+const headerNav = document.querySelector(".header-nav");
+
+const isTouchDevice = "ontouchstart" in window || navigator.maxTouchPoints > 0;
+
+const toggleMenu = () => {
+  hamburger.classList.toggle("active");
+  headerNav.classList.toggle("open");
+  document.body.classList.toggle("over-hide");
+
+  navGroups.forEach((nav) => nav.classList.remove("open"));
+};
+
+hamburger.addEventListener("click", toggleMenu);
+
+headerNav.addEventListener("click", (e) => {
+  const clickedInside = e.target.closest(".nav-wrapper");
+  if (!clickedInside) {
+    toggleMenu();
+  }
+});
+
+if (isTouchDevice) {
+  navGroups.forEach((navGroup) => {
+    navGroup.addEventListener("click", () => {
+      navGroup.classList.toggle("open");
+    });
+  });
+} else {
+  navGroups.forEach((navGroup) => {
+    navGroup.addEventListener("mouseenter", () => {
+      navGroup.classList.add("open");
+    });
+
+    navGroup.addEventListener("mouseleave", () => {
+      navGroup.classList.remove("open");
+    });
+  });
+}
